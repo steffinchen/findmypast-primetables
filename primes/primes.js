ea971bc2cac544cb37fbda0c321220ed2b73bdd4
Stefanie Wehrle <steffinchen@gmail.com>
2017-12-22 11:34:46 +0100

    tests for generating primes

--- primes/primes.js
+++ primes/primes.js
@@ -1,1 +1,7 @@
+'use strict'
 
+function generate(){}
+
+module.exports = {
+  generate
+}
--- primes/primes.spec.js
+++ primes/primes.spec.js
@@ -8,5 +8,44 @@ describe('Primes moduel', () => {
     it('should export a function', () => {
       expect(Primes.generate).to.be.a('function')
     })
+
+    it('generates the first 3 primes', () => {
+      expect(Primes.generate(3)).to.eql([2, 3, 5]);
+    });
+
+    it('generates the first 10 primes', () => {
+      expect(Primes.generate(10)).to.eql([
+        2,
+        3,
+        5,
+        7,
+        11,
+        13,
+        17,
+        19,
+        23,
+        29
+      ]);
+    });
+
+    it('generates 10000 primes in less than 1 sec', () => {
+      var start = process.hrtime();
+
+      var result = Primes.generate(10000);
+
+      expect(result).to.have.lengthOf(10000);
+      var elapsed = process.hrtime(start)[1] / 1000000; // divide by a million to get nano to milli
+      expect(elapsed).to.be.below(1000);
+    });
+
+    it('generates 20000 primes in less than 2 sec', () => {
+      var start = process.hrtime();
+
+      var result = Primes.generate(20000);
+
+      expect(result).to.have.lengthOf(20000);
+      var elapsed = process.hrtime(start)[1] / 1000000; // divide by a million to get nano to milli
+      expect(elapsed).to.be.below(2000);
+    });
   })
 })
