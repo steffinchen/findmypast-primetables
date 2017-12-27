2ee293f15db8053103a818f5c3b4257604d72001
Stefanie Wehrle <steffinchen@gmail.com>
2017-12-27 09:33:27 +0100

    implementation of isTruthy function

--- utils/utils.js
+++ utils/utils.js
@@ -1,1 +1,15 @@
 'use strict'
+
+/**
+ * Returns true for all truthy values,
+ * false otherwise.
+ * @param val: the value to test
+ * @return true if val is a truthy value
+ */
+function isTruthy(val){
+  return !!val;
+}
+
+module.exports = {
+  isTruthy
+}
