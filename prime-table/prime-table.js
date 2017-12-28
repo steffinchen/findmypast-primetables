c3a7c9351395ff9489572eed5bf0d73302dba17d
Stefanie Wehrle <steffinchen@gmail.com>
2017-12-28 18:03:19 +0100

    implementation of prime-table module

--- prime-table/prime-table.js
+++ prime-table/prime-table.js
@@ -1,5 +1,13 @@
 'use strict'
 
-function generatePrimeTable(){
+const MultiTable = require('../multiplication-table/multi-table');
+const Prime = require('../primes/primes')
 
+function generatePrimeTable(n){
+  let primes = Prime.generate(n);
+  return MultiTable.generate(primes);
+}
+
+module.exports = {
+  generatePrimeTable
 }
--- prime-table/prime-table.spec.js
+++ prime-table/prime-table.spec.js
@@ -24,7 +24,7 @@ describe('PrimeTable module', () => {
   });
 
   it('generates a table with lots of primes', () => {
-    let table = PrimeTable.generatePrimeTable(200)
+    let table = PrimeTable.generatePrimeTable(46);
     expect(table[0]).to.eql([null, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]);
   });
 
