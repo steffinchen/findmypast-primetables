a393d7242494acdff174ba2cfea572ac2cee6121
Stefanie Wehrle <steffinchen@gmail.com>
2017-12-29 17:24:36 +0100

    main component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import MultiplicationTableUI from './MultiplicationTableUI';
 
 class App extends React.Component {
   render() {
-    return <h1>Hello, world</h1>;
+    return <div>
+        <h1>Findmypast Prime Tables</h1>
+        <div>Enter a whole number > 0: <input></input><button>OK</button></div>
+        <MultiplicationTableUI />
+      </div>;
   }
 }
 
