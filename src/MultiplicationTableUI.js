07f221ab113fb6a2e633238a4d6f9c67c36f3e4b
Stefanie Wehrle <steffinchen@gmail.com>
2017-12-30 18:01:15 +0100

    multiplication table is only shown if there's an input > 0

--- src/MultiplicationTableUI.js
+++ src/MultiplicationTableUI.js
@@ -1,27 +1,36 @@
 import React from 'react';
 import range from 'lodash/range';
 import PrimeTable from './backend/prime-table/prime-table'
+import isUndefined from 'lodash/isUndefined';
 
 class MultiplicationTableUI extends React.Component {
   render() {
-    const primeTable = PrimeTable.generatePrimeTable(this.props.n);
-    return <table className="table table-bordered">
-      <thead>
-        <tr>
-        {range(primeTable[0].length).map((j) => <th key={'0_'+j}>{primeTable[0][j]}</th>)}
-      </tr>
-      </thead>
-      <tbody>
-        {
-          range(1, primeTable.length).map((i) => <tr key={'row-'+i}>
-            {range(primeTable[i].length).map((j) => {
-              const CellTag = j === 0 ? 'th' : 'td';
-              return <CellTag key={i+'_'+j}>{primeTable[i][j]}</CellTag>;
-            })}
-          </tr>)
-        }
-      </tbody>
-    </table>;
+    if (isUndefined(this.props.n) || this.props.n <= 0) {
+      return <div></div>;
+    } else {
+      const primeTable = PrimeTable.generatePrimeTable(this.props.n);
+      return <table className="table table-bordered">
+        <thead>
+          <tr>
+            {range(primeTable[0].length).map((j) => <th key={'0_' + j}>{primeTable[0][j]}</th>)}
+          </tr>
+        </thead>
+        <tbody>
+          {
+            range(1, primeTable.length).map((i) => <tr key={'row-' + i}>
+              {
+                range(primeTable[i].length).map((j) => {
+                  const CellTag = j === 0
+                    ? 'th'
+                    : 'td';
+                  return <CellTag key={i + '_' + j}>{primeTable[i][j]}</CellTag>;
+                })
+              }
+            </tr>)
+          }
+        </tbody>
+      </table>;
+    }
   }
 }
 
--- src/backend/prime-table/prime-table.spec.js
+++ src/backend/prime-table/prime-table.spec.js
@@ -28,4 +28,11 @@ describe('PrimeTable module', () => {
     expect(table[0]).to.eql([null, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]);
   });
 
+  it('generates a big table fast', () => {
+    var start = process.hrtime();
+    PrimeTable.generatePrimeTable(5000);
+    var elapsed = process.hrtime(start)[1] / 1000000; // divide by a million to get nano to milli
+    expect(elapsed).to.be.below(1000);
+  });
+
 });
